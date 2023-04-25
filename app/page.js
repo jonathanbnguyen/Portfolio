f33dd132d9af988a1ba5dd7226951771cf432b63
jonathan <jonathanbn29@gmail.com>
2023-04-24 21:51:24 -0500

    configured tailwindcss

--- app/page.js
+++ app/page.js
@@ -2,10 +2,10 @@
 
 export default function Home() {
 	return (
-		<main className='pb24'>
+		<main>
 			<div>
-				<h1 className="text-l text-slate-300 mb-7">Hello, my name is</h1>
-				<p className="mb-2 md:text-7xl text-4xl text-indigo-500 font-bold">
+				<h1>Hello, my name is</h1>
+				<p>
 					Jonathan Nguyen
 				</p>
 			</div>
