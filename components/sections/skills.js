e1ffe0fcb1a88207e19851e3975e7541826fb5dc
jonathan <jonathanbn29@gmail.com>
2023-04-29 20:46:21 -0500

    changed skills

--- components/sections/skills.js
+++ components/sections/skills.js
@@ -8,19 +8,17 @@ export default function Skills() {
     return (
         <Section id="skills" title="Technical Summary">
             <div className="flex flex-row justify-between flex-wrap">
-                {/* <div className="md:w-1/2 text-white mt-4 space-y-4">
-                    <p>
+                <div className="md:w-full lg:w-full space-y-4 mt-4 text-white">
+                    {/* <p>
                         Throughout my career, I have been able to learn multiple technology stacks
                         and softwares from internships, work, school/school projects, and personal projects.
                         With time and experience, I can learn anything thrown at me.
-                    </p>
-                </div> */}
+                    </p> */}
 
-                <div className="lg:m-auto">
                     {Titles.map(group =>
                         (
                             <div key={`${group.name}`}>
-                                <p className="font-semibold text-white mt-4">{group.name}</p>
+                                <p className="font-semibold text-white">{group.name}</p>
                                 <div className="flex flex-row flex-wrap">
                                     {group.values.map((skill) => (
                                         <SkillCard key={skill} title={skill} />
@@ -30,6 +28,10 @@ export default function Skills() {
                         ),
                     )}
                 </div>
+
+                {/* <div className="lg:m-auto">
+
+                </div> */}
             </div>
         </Section>
     )
--- components/skillscard.js
+++ components/skillscard.js
@@ -1,8 +1,7 @@
 export default function SkillCard({ title }) {
     return (
         <span
-            className="text-slate-200 text-sm mt-2 inline-block mr-2 last:mr-0 bg-blue-700 p-1.5 rounded"
-        >
+            className="text-white text-sm mt-2 inline-block mr-2 last:mr-0 bg-blue-700 p-1.5 rounded">
             {title}
         </span>
     );
