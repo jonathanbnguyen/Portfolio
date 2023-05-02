ca492214f3934510c041358234ffeda50acc702c
jonathan <jonathanbn29@gmail.com>
2023-05-02 13:10:24 -0500

    added to skills

--- components/data/projectdata.js
+++ components/data/projectdata.js
@@ -3,9 +3,7 @@ export const Group = [
         name: "MyPantry",
         description: 
             "MyPantry is a cross-platform mobile application that helps users with everyday within the kitchen. "
-            + "Inventory feature that can be added to by scanning barcode of items, searching food database, or manually. Each item has edit/delete option with full product information and nutrition facts. "
-            + "Recipe feature that can be used to search, save, and/or create recipes."
-            + "Spending Tracker feature that can be used to set monthly budgets and deduct by scanning receipt or adding manually and will notify when the budget is almost hit."
+            + "Features include a full inventory system, search/save/create recipes, and spending tracker."
         ,
         tech: [
             "JavaScript",
--- components/data/skillsdata.js
+++ components/data/skillsdata.js
@@ -4,7 +4,8 @@ export const Group = [
         values: [
             "C++",
             "C#",
-            "Python"
+            "Python",
+            "JavaScript"
         ]
     },
     {
