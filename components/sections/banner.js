afd09d05ab24284342e820bcdcd5d72359d9c22d
jonathan <jonathanbn29@gmail.com>
2023-05-11 00:51:34 -0500

    changed typewriter words

--- components/sections/banner.js
+++ components/sections/banner.js
@@ -24,7 +24,7 @@ export function Banner() {
 
                     <Typewriter
                         options={{
-                            strings: ["Listener.", "Learner.", "Developer."],
+                            strings: ["Learner.", "Builder", "Developer."],
                             autoStart: true,
                             loop: true,
                             deleteSpeed: 100,
